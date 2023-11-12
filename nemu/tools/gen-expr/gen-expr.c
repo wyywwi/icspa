db0a132aa4264895ea8136fa4bf8638e0ab59f10
tracer-ics2023 <tracer@njuics.org>
2023-11-12 15:54:31 +0800

    >  compile NEMU 202112014 张飞扬 Linux thinkbook16 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:54:31 up  4:57,  1 user,  load average: 0.27, 0.25, 0.24

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -58,7 +58,7 @@ static unsigned int gen_rand_expr() {
   uint32_t n = 0,a=0,b=0;
   switch(rand()%3){
   	case 0:
-  		n = rand()%65536;
+  		n = rand()%30;
   	  int n_len = countDigits(n);
       snprintf(buf + place,n_len+1,"%u",n);
       place += n_len;
