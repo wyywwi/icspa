00d12ff1209251c985d6b87f6f87a1013137e583
tracer-ics2023 <tracer@njuics.org>
2023-11-12 23:56:24 +0800

    >  compile NEMU 202112014 张飞扬 Linux thinkbook16 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:56:24 up 12:59,  1 user,  load average: 0.59, 0.68, 1.08

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -22,13 +22,6 @@
 
 static int is_batch_mode = false;
 
-void init_regex();
-void init_wp_pool();
-WP* new_wp(char *exp);
-void free_wp(WP *wp);
-void print_wp();
-void delete_wp(int N);
-
 /* We use the `readline' library to provide more flexibility to read from stdin. */
 static char* rl_gets() {
   static char *line_read = NULL;
