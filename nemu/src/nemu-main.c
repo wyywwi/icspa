5b9c3ce38c38d2c6b36b8ae986ed8eda33a60c47
tracer-ics2023 <tracer@njuics.org>
2023-11-14 19:33:41 +0800

    >  compile NEMU 202112014 张飞扬 Linux thinkbook16 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:33:41 up  3:25,  1 user,  load average: 0.21, 0.16, 0.17

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -28,21 +28,21 @@ int main(int argc, char *argv[]) {
 #else
   init_monitor(argc, argv);
 #endif
-  // int loop = 70;
-  // if(argc > 1){
-  //   sscanf(argv[1],"%d",&loop);
-  // }
-  // FILE* is_open = fopen("/home/kkoapbd/Desktop/ics2023/nemu/tools/gen-expr/input","r");
-  // if(is_open == NULL)panic("error for open");
-  // for(int i = 0;i<loop;i++){
-  //   uint32_t n;
-  //   bool success;
-  //   char buf[65536];
-  //   int is_sca = fscanf(is_open,"%u%s",&n,buf);
-  //   if(!is_sca)break;
-  //   uint32_t result = expr(buf,&success);
-  //   if(result != n)panic("Error at %s and result is %u but need to be %u\n",buf,result,n);
-  // }
+  int loop = 100;
+  if(argc > 1){
+    sscanf(argv[1],"%d",&loop);
+  }
+  FILE* is_open = fopen("/home/kkoapbd/Desktop/ics2023/nemu/tools/gen-expr/input","r");
+  if(is_open == NULL)panic("error for open");
+  for(int i = 0;i<loop;i++){
+    uint32_t n;
+    bool success;
+    char buf[65536];
+    int is_sca = fscanf(is_open,"%u%s",&n,buf);
+    if(!is_sca)break;
+    uint32_t result = expr(buf,&success);
+    if(result != n)panic("Error at %s and result is %u but need to be %u\n",buf,result,n);
+  }
   /* Start engine. */
   engine_start();
 
