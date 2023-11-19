4cb920fbaa2829ecee6ec9b7d30989de9b6c4d80
tracer-ics2023 <tracer@njuics.org>
2023-11-19 21:34:31 +0800

    >  compile NEMU 202112014 张飞扬 Linux thinkbook16 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:34:31 up 11:26,  2 users,  load average: 0.72, 0.58, 0.37

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,27 +5,53 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  int64_t count = 0;
+  while(*(s + count) != '\0')count++;
+  return count;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  int64_t count = 0;
+  while(*(src + count) != '\0'){
+    *(dst + count) = *(src + count);
+    count++;
+  }
+  *(dst + count) = '\0';
+  return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  int64_t count = 0;
+  while(*(src + count) != '\0' && count < n){
+    *(dst + count) = *(src + count);
+    count++;
+  }
+  *(dst + count) = '\0';
+  return dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  int64_t count1 = 0,count2 = 0;
+  while(*(dst+count1) != '\0')count1++;
+  while(*(src+count2) != '\0'){
+    *(dst + count1 + count2) = *(src + count2);
+    count2++;
+  }
+  *(dst + count1 + count2) = '\0';
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  //for (; *s1==*s2 && *s1; s1++, s2++);
+  while(*s1 == *s2 && *s1){s1++;s2++;}
+	return *(unsigned char *)s1 - *(unsigned char *)s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  const unsigned char *l=(void *)s1, *r=(void *)s2;
+	if (!n--) return 0;
+  while(*l && *r && n && *l == *r){l++;r++;n--;}
+	return *l - *r;
 }
 
 void *memset(void *s, int c, size_t n) {
