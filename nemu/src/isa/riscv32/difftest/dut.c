afe80767fc8a1b21e453fef7dae752d7c24563e9
tracer-ics2023 <tracer@njuics.org>
2023-11-21 19:58:27 +0800

    >  compile NEMU 202112014 张飞扬 Linux thinkbook16 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:58:27 up  1:22,  1 user,  load average: 0.29, 0.29, 0.30

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,7 +18,13 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  if(pc != ref_r->pc)return false;
+  else{
+    for(int i = 0;i<16;i++){
+      if(ref_r->gpr[i] != gpr(i))return false;
+    }
+  }
+  return true;
 }
 
 void isa_difftest_attach() {
