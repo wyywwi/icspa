56fe2fb2e100117b5554663be58f498fd0e4c541
tracer-ics2023 <tracer@njuics.org>
2023-11-21 11:41:32 +0800

    >  compile NEMU 202112014 张飞扬 Linux thinkbook16 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:41:32 up  1:25,  1 user,  load average: 0.12, 0.20, 0.21

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -52,7 +52,7 @@ void init_mem() {
 
 word_t paddr_read(paddr_t addr, int len) {
   #ifdef CONFIG_MTRACE
-  Log("read to memmory %08x for %d bytes",addr,len); 
+    Log("read to memmory %08x for %d bytes",addr,len); 
   #endif
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
@@ -62,7 +62,7 @@ word_t paddr_read(paddr_t addr, int len) {
 
 void paddr_write(paddr_t addr, int len, word_t data) {
   #ifdef CONFIG_MTRACE
-  Log("write to memmory %08x for %d bytes date %08x",addr,len,data); 
+    Log("write to memmory %08x for %d bytes date %08x",addr,len,data); 
   #endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
