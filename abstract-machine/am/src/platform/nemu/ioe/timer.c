fb66facfbe16525033e090286a7e22a77afb6da7
tracer-ics2023 <tracer@njuics.org>
2023-11-23 20:37:09 +0800

    >  compile NEMU 202112014 张飞扬 Linux thinkbook16 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:37:09 up  2:33,  1 user,  load average: 0.20, 0.24, 0.22

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,14 +1,18 @@
 #include <am.h>
 #include <nemu.h>
+
+static uint64_t start_time;
 void __am_timer_init() {
-  outl(RTC_ADDR,0);
-  outl(RTC_ADDR+4,0);
+  start_time = inl(RTC_ADDR + 4);
+  start_time <<= 32;
+  start_time += inl(RTC_ADDR + 4);
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uptime->us = inl(RTC_ADDR +4);
   uptime->us <<=32;
   uptime->us += inl(RTC_ADDR);
+  uptime->us -= start_time;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
