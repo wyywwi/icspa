7f75a70120d8fb7a0bd2de333696deb9927c747b
tracer-ics2023 <tracer@njuics.org>
2023-11-23 20:52:11 +0800

    >  compile NEMU 202112014 张飞扬 Linux thinkbook16 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:52:11 up  2:48,  1 user,  load average: 0.40, 0.32, 0.26

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,151 +5,119 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...)
-{
+int printf(const char *fmt, ...) {
   va_list args;
-  va_start(args, fmt);
+    va_start(args, fmt);
 
-  int output_count = 0;
-  const char *format_ptr = fmt;
+    int output_count = 0;
+    const char* format_ptr = fmt;
 
-  while (*format_ptr != '\0')
-  {
-    if (*format_ptr == '%')
-    {
-      format_ptr++;
-      if (*format_ptr == 'd')
-      {
-        // Handle %d format specifier
-        int num = va_arg(args, int);
-        char str[20];
-        sprintf(str, "%d", num);
-        char *str_ptr = str;
-        while (*str_ptr != '\0')
-        {
-          putch(*str_ptr);
-          str_ptr++;
-          output_count++;
+    while (*format_ptr != '\0') {
+        if (*format_ptr == '%') {
+            format_ptr++;
+            if (*format_ptr == 'd') {
+                // Handle %d format specifier
+                int num = va_arg(args, int);
+                char str[20];
+                sprintf(str, "%d", num);
+                char* str_ptr = str;
+                while (*str_ptr != '\0') {
+                    putch(*str_ptr);
+                    str_ptr++;
+                    output_count++;
+                }
+            } else if (*format_ptr == 's') {
+                // Handle %s format specifier
+                char* str = va_arg(args, char*);
+                char* str_ptr = str;
+                while (*str_ptr != '\0') {
+                    putch(*str_ptr);
+                    str_ptr++;
+                    output_count++;
+                }
+            }
+        } else {
+            putch(*format_ptr);
+            output_count++;
         }
-      }
-      else if (*format_ptr == 's')
-      {
-        // Handle %s format specifier
-        char *str = va_arg(args, char *);
-        char *str_ptr = str;
-        while (*str_ptr != '\0')
-        {
-          putch(*str_ptr);
-          str_ptr++;
-          output_count++;
-        }
-      }
-    }
-    else
-    {
-      putch(*format_ptr);
-      output_count++;
+
+        format_ptr++;
     }
 
-    format_ptr++;
-  }
-
-  va_end(args);
-  return output_count;
+    va_end(args);
+    return output_count;
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap)
-{
+int vsprintf(char *out, const char *fmt, va_list ap) {
   return vsnprintf(out, INT32_MAX, fmt, ap);
 }
 
-int sprintf(char *out, const char *fmt, ...)
-{
+int sprintf(char *out, const char *fmt, ...) {
   int ret;
-  va_list ap;
-  va_start(ap, fmt);
-  ret = vsprintf(out, fmt, ap);
-  va_end(ap);
-  return ret;
+	va_list ap;
+	va_start(ap, fmt);
+	ret = vsprintf(out, fmt, ap);
+	va_end(ap);
+	return ret;
 }
 
-int snprintf(char *out, size_t n, const char *fmt, ...)
-{
+int snprintf(char *out, size_t n, const char *fmt, ...) {
   int ret;
-  va_list ap;
-  va_start(ap, fmt);
-  ret = vsnprintf(out, n, fmt, ap);
-  va_end(ap);
-  return ret;
+	va_list ap;
+	va_start(ap, fmt);
+	ret = vsnprintf(out, n, fmt, ap);
+	va_end(ap);
+	return ret;
 }
 
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap)
-{
-  int count = 0;   // 用于计算写入缓冲区的字符数
-  int written = 0; // 用于记录已写入字符数
-
-  while (fmt[count] != '\0' && written < n - 1)
-  {
-    if (fmt[count] != '%')
-    {
-      // 普通字符直接复制到输出缓冲区
-      out[written] = fmt[count];
-      count++;
-      written++;
-    }
-    else
-    {
-      // 处理格式化指示符
-      count++;
-      if (fmt[count] == '\0')
-      {
-        break; // 格式化指示符不完整，退出循环
-      }
-
-      if (fmt[count] == '%')
-      {
-        // 转义的百分号
-        out[written] = '%';
-        count++;
-        written++;
-      }
-      else if (fmt[count] == 'd')
-      {
-        // 处理 %d 格式化指示符
-        int arg = va_arg(ap, int);
-        int digits = snprintf(out + written, n - written, "%d", arg);
-        if (digits >= n - written)
-        {
-          // 输出缓冲区空间不足，退出循环
-          break;
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+  int count1 = 0,count2 = 0;
+  while(*(fmt + count2) != '\0' && count2 < n){
+    if(*(fmt + count2) == '%'){
+      if(*(fmt + count2 + 1) == 'd'){
+        int arg = va_arg(ap,int);
+        int temp = arg;
+        int digits = 0;
+        if(temp == 0){
+          digits = 1;
+        }else{
+          while(temp != 0){
+            temp/= 10;
+            digits ++;
+          }
         }
-        written += digits;
-        count++;
-      }
-      else if (fmt[count] == 's')
-      {
-        // 处理 %s 格式化指示符
-        char *arg = va_arg(ap, char *);
-        int str_len = snprintf(out + written, n - written, "%s", arg);
-        if (str_len >= n - written)
-        {
-          // 输出缓冲区空间不足，退出循环
-          break;
+        if(count1 + digits < n){
+          int disvisor = 1;
+          for(int i = 0;i<digits -1;i++){
+            disvisor *= 10;
+          }
+          while(disvisor != 0){
+            int digit = arg/disvisor;
+            *(out + count1) = '0' + digit;
+            count1++;
+            arg %= disvisor;
+            disvisor /= 10;
+          }
         }
-        written += str_len;
-        count++;
       }
-      else
-      {
-        // 不支持的格式化指示符，忽略该指示符
-        count++;
+      else if(*(fmt + count2 + 1) == 's'){
+        char *arg = va_arg(ap,char *);
+        while(*arg != '\0' && count1 < n){
+          *(out + count1) = *arg;
+          count1++;
+          arg++;
+        }
       }
+      count2 += 2;
     }
+    else{
+      *(out + count1) = *(fmt + count2);
+      count1++;
+      count2++;
+    } 
   }
-
-  out[written] = '\0'; // 在输出缓冲区末尾添加字符串结束符
-
-  return written;
+  *(out + count1) = '\0';
+  return count1;
 }
 
 #endif
