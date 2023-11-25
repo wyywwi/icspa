eebec08b2c175d099fa4f9c2508eb4eb27d8a449
tracer-ics2023 <tracer@njuics.org>
2023-11-25 12:01:38 +0800

    >  compile NEMU 202112014 张飞扬 Linux thinkbook16 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:01:38 up 21 min,  1 user,  load average: 0.72, 0.38, 0.24

--- abstract-machine/scripts/native.mk
+++ abstract-machine/scripts/native.mk
@@ -11,7 +11,7 @@ AM_SRCS := native/trm.c \
            native/ioe/audio.c \
            native/ioe/disk.c \
 
-CFLAGS  += -fpie -g
+CFLAGS  += -fpie
 ASFLAGS += -fpie -pie
 comma = ,
 LDFLAGS_CXX = $(addprefix -Wl$(comma), $(LDFLAGS))
