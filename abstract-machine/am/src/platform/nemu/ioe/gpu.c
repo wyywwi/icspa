1e0736a6dfb30cbb540f498d96ce832cd7676e0d
tracer-ics2023 <tracer@njuics.org>
2023-12-06 15:54:39 +0800

    >  compile NEMU 202112014 张飞扬 Linux thinkbook16 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:54:39 up  1:01,  1 user,  load average: 1.16, 1.01, 0.88

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -5,13 +5,13 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-  int i;
-  int w = inw(VGACTL_ADDR + 2);
-  int h = inw(VGACTL_ADDR);
-  printf("%d,%d\n",w,h);
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for(i = 0;i<w*h;i++)fb[i] = i;
-  outl(SYNC_ADDR,1);
+  // int i;
+  // int w = inw(VGACTL_ADDR + 2);
+  // int h = inw(VGACTL_ADDR);
+  // printf("%d,%d\n",w,h);
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // for(i = 0;i<w*h;i++)fb[i] = i;
+  // outl(SYNC_ADDR,1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
