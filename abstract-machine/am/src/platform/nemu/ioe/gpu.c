2478408714b1a5300877858775c873de14193c6a
tracer-ics2023 <tracer@njuics.org>
2023-12-06 15:53:33 +0800

    >  compile NEMU 202112014 张飞扬 Linux thinkbook16 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:53:33 up  1:00,  1 user,  load average: 0.89, 0.92, 0.84

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -25,7 +25,7 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   uint32_t *pixels = ctl->pixels;
-  int w = inw(VGACTL_ADDR);
+  int w = inw(VGACTL_ADDR + 2);
   //int h = inw(VGACTL_ADDR);
   int base = ctl->y * w + ctl->x;
   for(int i = 0;i<ctl->w;i++){
